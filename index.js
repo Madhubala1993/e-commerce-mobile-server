074df65d67a2cd94aff55a50fed479c8df05f569
Madhubala1993 <madhu.a1993@gmail.com>
2022-04-06 13:25:02 +0530

    env added

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 const PORT = 5055;
 
-const mobiles = [
-  {
-    model: "OnePlus 9 5G",
-    img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    company: "Oneplus",
-  },
-  {
-    model: "Iphone 13 mini",
-    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-    company: "Apple",
-  },
-  {
-    model: "Samsung s21 ultra",
-    img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-    company: "Samsung",
-  },
-  {
-    model: "xiomi mi 11",
-    img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-    company: "xiomi",
-  },
-];
+app.use(express.json());
 app.use(cors());
+
+const MONGO_URL = process.env.MONGO_URL;
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected");
+  return client;
+}
+
+export const client = await createConnection();
 
 app.get("/", function (req, res) {
   res.send("Hello World 😊😊😊😊");
 });
 
-app.get("/mobiles", function (req, res) {
+app.get("/mobiles", async function (req, res) {
+  const mobiles = await client
+    .db("ecommerce")
+    .collection("mobiles")
+    .find({})
+    .toArray();
   res.send(mobiles);
+});
+
+app.post("/mobiles", async function (req, res) {
+  const data = req.body;
+  console.log(data);
+  const result = await client
+    .db("ecommerce")
+    .collection("mobiles")
+    .insertMany(data);
+  res.send(result);
 });
 
 app.listen(PORT, () => console.log("App started in ", PORT));
